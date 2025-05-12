2fb6421b59e976f13da8d89c7389f895f4f85081
Lorenzo <lorenzo.avalle@voxy.com>
2025-05-12 18:32:37 +0200

    remove unused stuff

--- app/routes/calls_.admin+/index.tsx
+++
@@ -1,9 +0,0 @@
-import { type KCDHandle } from '#app/types.ts'
-
-export const handle: KCDHandle = {
-	getSitemapEntries: () => null,
-}
-
-export default function NoCallSelected() {
-	return <div>Select a call</div>
-}
